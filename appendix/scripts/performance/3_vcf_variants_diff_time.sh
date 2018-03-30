945203d96a865d21ae6edc8128cdd70a9ec13941
Xin Huang <xin-huang@users.noreply.github.com>
2018-03-30 22:11:20 +0800

    Update 3_vcf_variants_diff_time.sh

--- appendix/scripts/performance/3_vcf_variants_diff_time.sh
+++ appendix/scripts/performance/3_vcf_variants_diff_time.sh
@@ -6,7 +6,7 @@ do
         do
 		sleep 10
                 echo "time: $j"
-                /usr/bin/time --verbose ../SeleDiff/build/install/SeleDiff/bin/SeleDiff compute-diff --vcf ./test_pop_2_ind_2000_snp_${var[$i]}.vcf --ind test_pop_2_ind_2000.ind --var test_pop_2_ind_2000_snp_${var[$i]}.var --time test_pop_2_ind_2000_snp_${var[$i]}.time --output test_pop_2_ind_2000_snp_${var[$i]}.selediff
+                /usr/bin/time --verbose ../../../build/install/SeleDiff/bin/SeleDiff compute-diff --vcf ./test_pop_2_ind_2000_snp_${var[$i]}.vcf --ind test_pop_2_ind_2000.ind --var test_pop_2_ind_2000_snp_${var[$i]}.var --time test_pop_2_ind_2000_snp_${var[$i]}.time --output test_pop_2_ind_2000_snp_${var[$i]}.selediff
                 echo
         done
 done
