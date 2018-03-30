91fe310f57b0922dcce048549dd9241b7ae89d0c
Xin Huang <xin-huang@users.noreply.github.com>
2018-03-30 22:13:09 +0800

    Update 11_vcf_individuals_var_time.sh

--- appendix/scripts/performance/11_vcf_individuals_var_time.sh
+++ appendix/scripts/performance/11_vcf_individuals_var_time.sh
@@ -6,7 +6,7 @@ do
         do
 		sleep 10
                 echo "time: $j"
-                /usr/bin/time --verbose ../SeleDiff/build/install/SeleDiff/bin/SeleDiff compute-var --vcf ./test_pop_2_ind_${ind[$i]}_snp_1000000.vcf --ind test_pop_2_ind_${ind[$i]}_snp_1000000.ind --output test_pop_2_ind_${ind[$i]}_snp_1000000.var
+                /usr/bin/time --verbose ../../../build/install/SeleDiff/bin/SeleDiff compute-var --vcf ./test_pop_2_ind_${ind[$i]}_snp_1000000.vcf --ind test_pop_2_ind_${ind[$i]}_snp_1000000.ind --output test_pop_2_ind_${ind[$i]}_snp_1000000.var
                 echo
         done
 done
