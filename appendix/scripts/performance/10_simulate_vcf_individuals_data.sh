2819620d6c68d4b50b840fedf01a1a35681cb671
Xin Huang <xin-huang@users.noreply.github.com>
2018-03-30 22:14:53 +0800

    Update 10_simulate_vcf_individuals_data.sh

--- appendix/scripts/performance/10_simulate_vcf_individuals_data.sh
+++ appendix/scripts/performance/10_simulate_vcf_individuals_data.sh
@@ -3,7 +3,7 @@ sigind=(1000 2000 5000 10000 20000 50000 100000)
 
 for ((i=0;i<7;i++))
 do
-        java -jar ../programs/VCFGenerator.jar 2 ${ind[$i]} 1000000 test_pop_2_ind_${ind[$i]}_snp_1000000
+        java -jar ../../programs/VCFGenerator.jar 2 ${ind[$i]} 1000000 test_pop_2_ind_${ind[$i]}_snp_1000000
         for ((j=0;j<${sigind[$i]};j++)) do echo "ind$j U pop0"; done > test_pop_2_ind_${ind[$i]}_snp_1000000.ind
         for ((j=${sigind[$i]};j<${ind[$i]};j++)) do echo "ind$j U pop1"; done >> test_pop_2_ind_${ind[$i]}_snp_1000000.ind
 done
