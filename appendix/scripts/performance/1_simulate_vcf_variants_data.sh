0f7e70fd48c731faaeb00cf845123cde898f6368
Xin Huang <xin-huang@users.noreply.github.com>
2018-03-30 22:06:31 +0800

    Update 1_simulate_vcf_variants_data.sh

--- appendix/scripts/performance/1_simulate_vcf_variants_data.sh
+++ appendix/scripts/performance/1_simulate_vcf_variants_data.sh
@@ -2,5 +2,5 @@ var=(10000 20000 50000 100000 200000 500000 1000000 2000000 5000000 10000000 100
 
 for ((i=0;i<11;i++))
 do
-        java -jar ../programs/VCFGenerator.jar 2 2000 ${var[$i]} test_pop_2_ind_2000_snp_${var[$i]}
+        java -jar ../../programs/VCFGenerator.jar 2 2000 ${var[$i]} test_pop_2_ind_2000_snp_${var[$i]}
 done
