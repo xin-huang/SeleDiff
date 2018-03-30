e6d44ec09ae4891567cd6e97602a231bf6a3996e
xin-huang <xin.huang07@gmail.com>
2018-03-30 18:26:51 +0800

    20180330 Add scripts

---
+++ appendix/programs/0_download_programs.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/szpiech/selscan
+git clone https://github.com/anbena/4p
+wget -c http://benhaller.com/slim/SLiM.zip
---
+++ appendix/scripts/Performance/8_eigen_individuals_var_time.sh
@@ -0,0 +1,12 @@
+ind=(2000 4000 10000 20000 40000 100000 200000)
+
+for ((i=0;i<7;i++))
+do
+        for ((j=0;j<3;j++))
+        do
+		sleep 10
+                echo "time: $j"
+                /usr/bin/time --verbose ../SeleDiff/build/install/SeleDiff/bin/SeleDiff compute-var --geno ./test_pop_2_ind_${ind[$i]}_snp_1000000.geno --ind test_pop_2_ind_${ind[$i]}_snp_1000000.ind --snp ./test_pop_2_ind_${ind[$i]}_snp_1000000.snp --output test_pop_2_ind_${ind[$i]}_snp_1000000.var
+                echo
+        done
+done
